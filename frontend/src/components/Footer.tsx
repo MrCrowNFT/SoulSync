e347e86929bb4d8edc8085ef74cdd16c8248de72
MrCrowNFT <mrcrownft@gmail.com>
2025-02-17 07:13:44 -0300

    Footer Layout

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,91 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        {/*Grid */}
+        <div>
+          <div>
+            <h3>About</h3>
+            <ul>
+              <li>
+                <a href="">Random</a>
+              </li>
+              <li>
+                <a href="">Random</a>
+              </li>
+              <li>
+                <a href="">Random</a>
+              </li>
+              <li>
+                <a href="">Random</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h3>Privacy</h3>
+            <ul>
+              <li>
+                <a href="">Random</a>
+              </li>
+              <li>
+                <a href="">Random</a>
+              </li>
+              <li>
+                <a href="">Random</a>
+              </li>
+              <li>
+                <a href="">Random</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h3>Terms of service</h3>
+            <ul>
+              <li>
+                <a href="">Random</a>
+              </li>
+              <li>
+                <a href="">Random</a>
+              </li>
+              <li>
+                <a href="">Random</a>
+              </li>
+              <li>
+                <a href="">Random</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h3>Crisis resources</h3>
+            <ul>
+              <li>
+                <a href="">Random</a>
+              </li>
+              <li>
+                <a href="">Random</a>
+              </li>
+              <li>
+                <a href="">Random</a>
+              </li>
+              <li>
+                <a href="">Random</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        {/*Social media */}
+        <div>
+          <h2>Follow Us!</h2>
+          <img loading="lazy" src="" alt="" />
+        </div>
+        {/*Logo and trademark */}
+        <div>
+          <img loading="lazy" src="" alt="" />
+          <p></p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
