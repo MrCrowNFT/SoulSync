0bd1e16f50fac3c008b620a29c9d6d743ad32dea
MrCrowNFT <mrcrownft@gmail.com>
2025-02-17 09:35:32 -0300

    router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,23 @@
-import './App.css'
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Chatbot from "./pages/Chatbot";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
-
   return (
     <>
-      
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/chat" element={<Chatbot />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
