238342c828f8ea300420f2f1962be001b41d15c5
MrCrowNFT <mrcrownft@gmail.com>
2025-02-17 09:29:31 -0300

    Basic Pages

---
+++ frontend/src/pages/Signup.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+const Signup = () => {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  );
+};
+export default Signup;
