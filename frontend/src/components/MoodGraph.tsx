2aac9615a5c0aaa08e65f15748fd2872d8b9cb17
MrCrowNFT <mrcrownft@gmail.com>
2025-02-19 10:37:36 -0300

    Mood graph debugging

--- frontend/src/components/MoodGraph.tsx
+++ frontend/src/components/MoodGraph.tsx
@@ -12,7 +12,7 @@ import {
 import { FunctionComponent } from "react";
 import { LineProp } from "../types/Graph.ts";
 
-const MoodGraph: FunctionComponent<LineProp> = ({data}) => {
+const MoodGraph: FunctionComponent<LineProp> = ({ data }) => {
   Chart.register(
     CategoryScale,
     LinearScale,
@@ -23,6 +23,10 @@ const MoodGraph: FunctionComponent<LineProp> = ({data}) => {
     Legend
   );
 
-  return <Line options={data.options} data={data.data}></Line>;
+  return (
+    <div className="w-full md:w-1/2 p-4 ml-10">
+      <Line options={data.options} data={data.data}></Line>
+    </div>
+  );
 };
 export default MoodGraph;
