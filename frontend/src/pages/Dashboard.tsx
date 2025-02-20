5751e9546216509f48625a6d5264e7e7c14643c5
MrCrowNFT <mrcrownft@gmail.com>
2025-02-20 07:19:11 -0300

    Added assesment to dashboard

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -3,17 +3,19 @@ import Footer from "../components/Footer";
 import UserCard from "../components/UserCard";
 import { mockUser } from "../mocks/user.mock";
 import MoodGraph from "../components/MoodGraph";
-import {moodGraphMock} from "../mocks/graph.mock";
-
+import { moodGraphMock } from "../mocks/graph.mock";
+import Assessment from "../components/Assessment";
+import { mockAssessmentData } from "../mocks/assesment.mock";
 
 const Dashboard = () => {
-    return (
-        <>
-          <Navbar />
-          <UserCard user={mockUser}/>
-          <MoodGraph data={moodGraphMock}/>
-          <Footer />
-        </>
-      );
+  return (
+    <>
+      <Navbar />
+      <UserCard user={mockUser} />
+      <MoodGraph data={moodGraphMock} />
+      <Assessment assessment={mockAssessmentData} />
+      <Footer />
+    </>
+  );
 };
 export default Dashboard;
