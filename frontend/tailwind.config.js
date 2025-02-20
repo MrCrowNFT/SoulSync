1618ff247a6977ef81713143dd36b523db211759
MrCrowNFT <mrcrownft@gmail.com>
2025-02-20 10:05:50 -0300

    tailwind config

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class", // or 'media' if you prefer system settings
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode: "class",
+};
