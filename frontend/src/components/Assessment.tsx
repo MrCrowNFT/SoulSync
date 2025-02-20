530cfc75e3905f719f36e88703fdb24c03fcc7cb
MrCrowNFT <mrcrownft@gmail.com>
2025-02-20 07:16:56 -0300

    assessment mock

--- frontend/src/components/Assessment.tsx
+++ frontend/src/components/Assessment.tsx
@@ -1,11 +1,17 @@
 import { FunctionComponent } from "react";
-import { AssessmentProp } from "../types/Assesment";
+import { AssessmentProp } from "../types/Assessment";
 
 const Assessment: FunctionComponent<AssessmentProp> = ({ assessment }) => {
+  const formattedTimestamp = new Date(assessment.timestamp).toLocaleString();
+  
   return (
     <div className="w-full md:w-1/2 p-4 mr-10">
       <h2 className="text-xl font-bold">AI Assessment</h2>
-      <p className="text-gray-700">{assessment}</p>
+      <p className="text-gray-700">{assessment.assessment}</p>
+      <div>
+        <button>New Assessment</button>
+        <p>{formattedTimestamp}</p>
+      </div>
     </div>
   );
 };
--- frontend/src/types/Assesment.ts
+++
@@ -1,3 +0,0 @@
-export interface AssessmentProp {
-    assessment: string;
-  }
