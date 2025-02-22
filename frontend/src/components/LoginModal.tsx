a955c63bcf54b02e903157ea6be2e17cd7627bdb
MrCrowNFT <mrcrownft@gmail.com>
2025-02-22 09:37:49 -0300

    Login Modal component

--- frontend/src/components/LoginModal.tsx
+++ frontend/src/components/LoginModal.tsx
@@ -1,4 +1,34 @@
+import React, { useState } from "react";
+
 const LoginModal = () => {
-  return <></>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState<string | null>(null);
+
+  const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <h1>Log In</h1>
+      <form onSubmit={handleLogin}>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 };
 export default LoginModal;
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,11 +1,13 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import LoginModal from "../components/LoginModal";
 
 
 const Login = () => {
     return (
         <>
           <Navbar />
+          <LoginModal/>
           <Footer />
         </>
       );
