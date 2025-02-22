9e4bbfe479df8b14887d6745913fe2c42afb0a9d
MrCrowNFT <mrcrownft@gmail.com>
2025-02-22 12:00:25 -0300

    minor adjustment navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -5,11 +5,11 @@ const Navbar = () => {
     <div className="sticky top-0 h-auto bg-[#6C9BCF] p-6 text-[#333333] font-mono">
       <div className="flex items-center justify-between">
         {/*Logo */}
-        <div className="flex items-center gap-10">
+        <div className="flex items-center gap-10 ml-5">
           <img className="logo" loading="lazy" src="" alt="logo" />
           {/*Nav */}
           <nav>
-            <ul className="flex gap-3">
+            <ul className="flex gap-6">
               <li>
                 <Link to="/home" className="custom-link">
                   Home
@@ -29,7 +29,7 @@ const Navbar = () => {
           </nav>
         </div>
         {/*Login Signup */}
-        <div className="flex justify-end space-x-4">
+        <div className="flex justify-end space-x-4 gap-6 mr-5">
           <Link to="/login" className="custom-link">
             Login
           </Link>
