a2ccf575cb66a5a204d509e28748d6632b7e1090
MrCrowNFT <mrcrownft@gmail.com>
2025-02-24 08:41:18 -0300

    Mood entry scheema adjustments

--- backend/module/moodEntry.model.ts
+++ backend/module/moodEntry.model.ts
@@ -2,10 +2,12 @@ import mongoose from "mongoose";
 
 const moodEntryScheema = new mongoose.Schema(
   {
+    _id: mongoose.Schema.Types.ObjectId,
     userId: {
-      type: mongoose.Schema.Types.ObjectId,// Reference to the user
+      type: mongoose.Schema.Types.ObjectId, // Reference to the user
       ref: "User",
     },
+    note: String, //will also save the recieved string, for testing porpuses
     mood: Number, // scale for mood from 1-5 (very_sad=1, very_happy=5)
     // this is just a prototype, need further investigation for improvment
   },
