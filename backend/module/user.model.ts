91098928bdbec35ed9b4acb92809804ce1763622
MrCrowNFT <mrcrownft@gmail.com>
2025-02-24 09:29:01 -0300

    Password hashing user model

--- backend/module/user.model.ts
+++ backend/module/user.model.ts
@@ -1,8 +1,12 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userScheema = new mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
+  name: { String, required: true },
+  lastName: { String, required: true },
   username: { String, required: true },
+  gender: { String, required: true },
   email: { String, required: true },
   passwordHash: { String, required: true },
   photo: String,
@@ -23,6 +27,17 @@ const userScheema = new mongoose.Schema({
     ref: "Preferences",
   },
 });
+
+//hash the password before storing it in the database, pre middleware hook runs before saving
+userScheema.pre("save", async function (next) {
+    try {
+      // only hash the password if is new/modified
+      if (!this.isModified("password")) return next();
+      this.passwordHash = await bcrypt.hash(this.passwordHash, 10);
+    } catch (error) {
+      next(error);
+    }
+  });
 
 const User = mongoose.model("User", userScheema);
 
