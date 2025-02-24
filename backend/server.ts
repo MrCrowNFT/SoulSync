3b0a0789e539c19d3196394e243e2af7d1a09c2e
MrCrowNFT <mrcrownft@gmail.com>
2025-02-24 06:41:05 -0300

    Mongo db connection

--- backend/server.ts
+++ backend/server.ts
@@ -1,9 +1,11 @@
 import app from "./app";
 import dotenv from "dotenv";
+import connectDb from "./config/database";
 
 dotenv.config();
 const PORT = process.env.PORT || 5500;
 
 app.listen(PORT, () => {
+  connectDb();
   console.log("Server started at http://localhost:" + PORT);
 });
