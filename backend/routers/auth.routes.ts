919f582b7b08629be42cec2ee89f05e8b5fb8089
MrCrowNFT <mrcrownft@gmail.com>
2025-02-26 10:08:51 -0300

    Auth router updated

--- backend/routers/auth.routes.ts
+++ backend/routers/auth.routes.ts
@@ -1,11 +1,15 @@
 import express from "express";
+import { login, logout } from "../controllers/auth.controller";
 
 const authRouter = express.Router();
 
 // User login
-authRouter.post("/login", );
+authRouter.post("/login", login);
+
+//refresh access token 
+authRouter.post("/refresh-token", )
 
 // User logout
-authRouter.post("/logout", );
+authRouter.post("/logout", logout);
 
 export default authRouter;
