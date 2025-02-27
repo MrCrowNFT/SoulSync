0dafdbd961368851365b5f21fc6ba59937baf535
MrCrowNFT <mrcrownft@gmail.com>
2025-02-27 08:16:22 -0300

    Updated preferences router

--- backend/routers/preferences.routes.ts
+++ backend/routers/preferences.routes.ts
@@ -1,17 +1,23 @@
 import express from "express";
+import {
+  createPreferences,
+  deletePreferences,
+  getPreferences,
+  updatePreferences,
+} from "../controllers/preferences.controller";
 
 const preferencesRouter = express.Router();
 
 // Create or update a user’s preferences
-preferencesRouter.post("/:userId");
+preferencesRouter.post("/:userId", createPreferences);
 
 // Fetch a user’s preferences
-preferencesRouter.get("/:userId");
+preferencesRouter.get("/:userId", getPreferences);
 
 //update user preferences
-preferencesRouter.put("/:userId");
+preferencesRouter.put("/:userId", updatePreferences);
 
 // Delete a user’s preferences
-preferencesRouter.delete("/:userId");
+preferencesRouter.delete("/:userId", deletePreferences);
 
 export default preferencesRouter;
