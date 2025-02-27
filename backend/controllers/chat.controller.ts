0582d10165cff4edcba6e3b13bdddb5eb621d0ec
MrCrowNFT <mrcrownft@gmail.com>
2025-02-27 09:43:38 -0300

    chat controller layout

--- backend/controllers/chat.controller.ts
+++ backend/controllers/chat.controller.ts
@@ -1,7 +1,10 @@
 import mongoose from "mongoose";
 import { Request, Response } from "express";
-import ChatEntry from "../module/ChatEntry.model";
+import ChatEntry from "../module/chatEntry.model";
 
-export const getChatHistory= async (req: Request, res: Response) =>{}
+export const getChatEntries = async (req: Request, res: Response) => {};
 
-export const newCha
+export const newChatHistory = async (req: Request, res: Response) => {};
+
+//delete all entries for userId -> should only happen when user desire or when account is deleted
+export const deleteUserChatEntries = async (req: Request, res: Response) => {};
