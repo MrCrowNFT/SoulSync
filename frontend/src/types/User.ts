34dd8fe2baf7dbc4acd0dd7a8cf5cb4bdc2ba98d
MrCrowNFT <mrcrownft@gmail.com>
2025-03-04 19:02:56 -0300

    User signup interface

--- frontend/src/types/User.ts
+++ frontend/src/types/User.ts
@@ -10,13 +10,14 @@ export interface UserCardProps {
 
 //New user profile
 //todo: replace the old one
-export interface User{
-  firstName: string,
-  lastName: string,
-  email: string,
-  username: string,
-  password: string,
-  confirmPassword: string,
-  birthDate: string,
-  gender: string,
+export interface UserSignupInput {
+  name: string;
+  lastName: string;
+  username: string;
+  gender: 'male' | 'female' | 'non-binary' | 'other' | 'prefer-not-to-say';
+  email: string;
+  password: string;
+  confirmPassword: string; // Added for password confirmation
+  photo?: string; // Optional profile photo
+  birthDate: string; // Date as ISO string (YYYY-MM-DD)
 }
